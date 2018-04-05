fcf4a8fda3bb87b5b7e9388cb0f6b6eb9e838f58
Boba <slobodan@agavelab.com>
2018-04-04 23:45:05 -0500

    4aprhome

--- clockfour.js
+++ clockfour.js
@@ -43,18 +43,6 @@ let curentHourWorld = 0;
 let imageLatRound = 0;
 let imageLonRound = 0;
 
-// function DisplayCurrentTime() {
-//         let date = new Date();
-//         let hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
-//         let am_pm = date.getHours() >= 12 ? "PM" : "AM";
-//         hours = hours < 10 ? "0" + hours : hours;
-//         let minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-//         let seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
-//         time = hours + ":" + minutes + ":" + seconds + " " + am_pm;
-//         let lblTime = document.getElementById("lblTime");
-//         lblTime.innerHTML = time;
-//     };
-
 
 function getWidthHeight() {
   theCSSpropWidth = window.getComputedStyle(image,null).getPropertyValue("width");
@@ -91,9 +79,27 @@ function displayLonLat(e) {
   }
 }
 
+function displayZoomed() {
+  if(zoombool) {
+    getWidthHeight();
+    positionYZoom = e.pageY - imageOffsetTop;
+    positionXZoom = e.pageX - imageOffsetLeft;
+    imageLatZoom = (maxlat) - ((positionYZoom/heightDevider) * 0.18);
+    imageLonZoom = ((positionXZoom/widthDevider) * 0.36 - (-minlon));
+    document.documentElement.style.setProperty("--pageX", e.pageX + suffix);
+    document.documentElement.style.setProperty(`--pageY`, e.pageY + suffix);
+    document.querySelector('.spanLat').innerHTML = imageLatZoom.toFixed(2);
+    document.querySelector('.spanLon').innerHTML = imageLonZoom.toFixed(2);
+    imageLat = imageLatZoom.toFixed(2);
+    imageLon = imageLonZoom.toFixed(2);
+    imageLatRound = imageLatZoom.toFixed(2);
+    imageLonRound = imageLonZoom.toFixed(2);
+  }
+}
+zoomedpic.addEventListener('click', displayZoomed);
+
 function displayOn() {
   if (!window.matchMedia("(max-width: 1000px)").matches) {
-    document.querySelector('.movingDivmax1000').style.display = "none";
     document.querySelector('.movingDiv').style.display = "block";
   }
 }
@@ -108,96 +114,31 @@ image.addEventListener("mouseover", displayOn);
 image.addEventListener("mouseout", displayOff);
 
 
-function displayLonLat1000px(e) {
-  if (!zoombool && window.matchMedia("(max-width: 1000px)").matches) {
-    getWidthHeight();
-    fetch(` https://maps.googleapis.com/maps/api/geocode/json?latlng=${imageLat},${imageLon}&key=AIzaSyAhbhZNE6A-Zcg49SMCyO7r_lH4MCDylRc `)
-    .then(response => response.json())
-    .then(cityName => {
-      worldPlace = cityName.results[0].address_components[1].short_name;
-      document.querySelector(".cityCorner1000").innerHTML = `${worldPlace}`;
-    })
-    fetch(` https://maps.googleapis.com/maps/api/timezone/json?location=${imageLat},${imageLon}&timestamp=1331161200&key=AIzaSyANpHwd0ZvP_2qrvqEEp-5l6NS3LkwxSbY `)
-    .then(response => response.json())
-    .then(world => offsetWorld = world.rawOffset)
-    .then(() => {
-      const timeGuadalajara = new Date().getHours()
-      const dayNow = new Date().getDay()
-      const offsetHours = (offsetWorld/3600);
-      console.log("timeWorld",timeWorld , offsetHours);
-      if ((offsetHours + timeWorld) > 23) {
-         curentDay = dayNow + 1
-       } else if ((offsetHours + timeWorld) < 1) {
-         curentDay = dayNow - 1
-       } else {
-         curentDay = dayNow
-       }
-
-       console.log("curentDay",curentDay);
-       switch (curentDay) {
-         case 0:
-         day = "Sunday";
-         break;
-         case 1:
-         day = "Monday";
-         break;
-         case 2:
-         day = "Tuesday";
-         break;
-         case 3:
-         day = "Wednesday";
-         break;
-         case 4:
-         day = "Thursday";
-         break;
-         case 5:
-         day = "Friday";
-         break;
-         case 6:
-         day = "Saturday";
-       }
-    })
-    .then(() => {
-    document.querySelector('.movingDivmax1000').style.display = "block";
-    positionY = e.pageY - imageOffsetTop;
-    positionX = e.pageX - imageOffsetLeft;
-    imageLat = (50 - positionY/heightDevider) * 1.8;
-    imageLon = (positionX/widthDevider - 50) * 3.6;
-    document.querySelector('.spanLat1000').innerHTML = imageLat.toFixed(2);
-    document.querySelector('.spanLon1000').innerHTML = imageLon.toFixed(2);
-    document.querySelector('.cornerTemp1000').innerHTML = Math.round(wheatherAllWorld) + "C";
-    document.querySelector('.cornerTempF1000').innerHTML = Math.round(weatherAllWorldF) + "F";
-    document.querySelector('.cornerDay1000').innerHTML = day;
-    const nowWorld = new Date();
-    const minsWorld = nowWorld.getMinutes() < 10 ? "0" + nowWorld.getMinutes() : nowWorld.getMinutes();
-    curentMin = minsWorld;
-    const hourWorld = nowWorld.getHours();
-    offsetHoursWorld = (offsetWorld / 3600);
-    const d = new Date();
-    const guadalajaraOffsetHours = d.getTimezoneOffset();
-    guadalajaraHours = (guadalajaraOffsetHours / 60);
-    curentHourWorld = (hourWorld + offsetHoursWorld + guadalajaraHours + 1);
-    if (curentHourWorld >= 24) {
-       let nextDay = curentHourWorld - 24
-       document.querySelector(".hoursWorld").innerHTML = `H${nextDay}`;
-       curentHour = nextDay;
-    }
-    else if (curentHourWorld < 0) {
-      let previousDay = curentHourWorld + 24
-      document.querySelector(".hoursWorld").innerHTML = `H${previousDay}`;
-      curentHourWorld = previousDay;
-    }
-     else {
-       document.querySelector(".hoursWorld").innerHTML = `H${curentHourWorld}`;
-       curentHour = curentHourWorld;
-     }
-    document.querySelector(".minutesWorld").innerHTML = `M${minsWorld}`;
-  });
-}
-}
-image.addEventListener("click", displayLonLat1000px);
-
-function getTimeWorld(){
+function getTimeWorld(e){
+  if (zoombool) {
+  positionYZoom = e.pageY - imageOffsetTop;
+  positionXZoom = e.pageX - imageOffsetLeft;
+  imageLatZoom = (maxlat) - ((positionYZoom/heightDevider) * 0.18);
+  imageLonZoom = ((positionXZoom/widthDevider) * 0.36 - (-minlon));
+  document.documentElement.style.setProperty("--pageX", e.pageX + suffix);
+  document.documentElement.style.setProperty(`--pageY`, e.pageY + suffix);
+  document.querySelector('.spanLat').innerHTML = imageLatZoom.toFixed(2);
+  document.querySelector('.spanLon').innerHTML = imageLonZoom.toFixed(2);
+  imageLat = imageLatZoom.toFixed(2);
+  imageLon = imageLonZoom.toFixed(2);
+  imageLatRound = imageLatZoom.toFixed(2);
+  imageLonRound = imageLonZoom.toFixed(2);
+  } else {
+  getWidthHeight();
+  positionY = e.pageY - imageOffsetTop;
+  positionX = e.pageX - imageOffsetLeft;
+  imageLat = (50 - positionY/heightDevider) * 1.8;
+  imageLon = (positionX/widthDevider - 50) * 3.6;
+  document.documentElement.style.setProperty("--pageX", e.pageX + suffix);
+  document.documentElement.style.setProperty(`--pageY`, e.pageY + suffix);
+  document.querySelector('.spanLat').innerHTML = imageLat.toFixed(2);
+  document.querySelector('.spanLon').innerHTML = imageLon.toFixed(2);
+  }
   fetch(` https://maps.googleapis.com/maps/api/geocode/json?latlng=${imageLat},${imageLon}&key=AIzaSyAhbhZNE6A-Zcg49SMCyO7r_lH4MCDylRc `)
   .then(response => response.json())
   .then(function(cityName) {
@@ -251,7 +192,6 @@ function getTimeWorld(){
     document.querySelector('.cornerTemp1000').innerHTML = Math.round(wheatherAllWorld) + "C";
     document.querySelector('.cornerTempF1000').innerHTML = Math.round(weatherAllWorldF) + "F";
     document.querySelector('.cornerDay1000').innerHTML = day;
-
   const nowWorld = new Date();
   const minsWorld = nowWorld.getMinutes();
   curentMin = minsWorld;
@@ -288,21 +228,14 @@ function imageClick(e , i) {
     imageLon = (positionX/widthDevider - 50) * 3.6;
     imageLatRound = imageLat.toFixed(2);
     imageLonRound = imageLon.toFixed(2);
-    console.log(imageLat , imageLon);
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${imageLat}&lon=${imageLon}&units=metric&APPID=261e313010ab3d43b1344ab9eba64cfa`)
     .then(response => response.json())
     .then(function(data) {
       wheatherAllWorld = data.main.temp ;
-      document.querySelector(".temp-AllWorld").innerHTML = `${Math.round(wheatherAllWorld)}`;
       weatherAllWorldF = (wheatherAllWorld * 1.8)+32;
-      document.querySelector(".tempF-AllWorld").innerHTML = `${Math.round(weatherAllWorldF)}`;
       wheatherIconWorld = data.weather[0].icon;
-      document.querySelector(".icon-AllWorld").innerHTML = `<img class="icon-Img-Tokyo" src="./content/${wheatherIconWorld}.png" width="70px" height="70px">`;
-      document.querySelector(".day-AllWorld").innerHTML = `${day}`;
     })
-    .then(() => {
-      getTimeWorld();
-    })
+    .then(() => getTimeWorld(e))
     .then(() => {
       fetch(` https://maps.googleapis.com/maps/api/geocode/json?latlng=${imageLat},${imageLon}&key=AIzaSyAhbhZNE6A-Zcg49SMCyO7r_lH4MCDylRc `)
       .then(response => response.json())
@@ -408,21 +341,16 @@ function zoom (e) {
   zoomedpic.addEventListener("click", zoomout);
 
   function zoomedAddToList(e) {
-    console.log(e.type);
     if (!e.ctrlKey && zoombool) {
       getWidthHeight();
       fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${imageLat}&lon=${imageLon}&units=metric&APPID=261e313010ab3d43b1344ab9eba64cfa`)
       .then(response => response.json())
-      .then(function(data) {
+      .then(data => {
         wheatherAllWorld = data.main.temp ;
-        document.querySelector(".temp-AllWorld").innerHTML = `${Math.round(wheatherAllWorld)}`;
         weatherAllWorldF = (wheatherAllWorld * 1.8)+32;
-        document.querySelector(".tempF-AllWorld").innerHTML = `${Math.round(weatherAllWorldF)}`;
         wheatherIconWorld = data.weather[0].icon;
-        document.querySelector(".icon-AllWorld").innerHTML = `<img class="icon-Img-Tokyo" src="./content/${wheatherIconWorld}.png" width="70px" height="70px">`;
-        document.querySelector(".day-AllWorld").innerHTML = `${day}`;
       })
-      .then(() => getTimeWorld())
+      .then(() => getTimeWorld(e))
       .then(() => {
         fetch(` https://maps.googleapis.com/maps/api/geocode/json?latlng=${imageLat},${imageLon}&key=AIzaSyAhbhZNE6A-Zcg49SMCyO7r_lH4MCDylRc `)
         .then(response => response.json())
@@ -485,6 +413,17 @@ function zoom (e) {
   }
   zoomedpic.addEventListener("click", zoomedAddToList);
 
+  // function DisplayCurrentTime() {
+  //         let date = new Date();
+  //         let hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+  //         let am_pm = date.getHours() >= 12 ? "PM" : "AM";
+  //         hours = hours < 10 ? "0" + hours : hours;
+  //         let minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  //         let seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+  //         time = hours + ":" + minutes + ":" + seconds + " " + am_pm;
+  //         let lblTime = document.getElementById("lblTime");
+  //         lblTime.innerHTML = time;
+  //     };
 
 
   // const secondHandWorld = document.querySelector('.second-handWorld');
